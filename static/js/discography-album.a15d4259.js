6e36de9ad94f0e66d31839d1e33bb1f37068e108
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-02 17:35:08 +0000

    Deploy hearts-round/hearts-round.github.io to github.com/hearts-round/hearts-round.github.io.git:master

--- static/js/discography-album.a15d4259.js
+++ static/js/discography-album.a15d4259.js
@@ -1,17 +1,1 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[3],{
-
-/***/ 108:
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-
-var Album = function () { return (react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "album")); };
-/* harmony default export */ __webpack_exports__["default"] = (Album);
-
-
-/***/ })
-
-}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{108:function(n,e,t){"use strict";t.r(e);var u=t(1),a=t.n(u);e.default=function(){return a.a.createElement("div",null,"album")}}}]);

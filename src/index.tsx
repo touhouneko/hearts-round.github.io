4a451c40c8ed4a6f199c86f58da474f931a5e045
fa93hws <wjun0912@gmail.com>
2018-11-12 22:52:58 +1100

    add html5 shiv

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import 'babel-polyfill';
+import 'html5shiv/dist/html5shiv.min.js'
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
